b00964f49586af44c26d2863664025c5fa17bde8
ManashNepal <manash.nepal111@gmail.com>
2024-04-14 15:26:40 +0530

    add functionality to toggle navbar

--- script.js
+++ script.js
@@ -10,3 +10,32 @@ window.addEventListener("load",function(){
     preloader.classList.add("loaded");
     document.body.classList.add("loaded");
 });
+
+
+// Mobile navbar
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNav = function(){
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNav);
+ 
+//active header when window scroll down to 100px
+
+const header = document.querySelector("[data-header]");
+
+const activeElementOnScroll = function(){
+    if(window.scrollY>100){
+        header.classList.add("active");
+    }
+    else{
+        header.classList.remove("active");
+    }
+}
+
+window.addEventListener("scroll",activeElementOnScroll);
